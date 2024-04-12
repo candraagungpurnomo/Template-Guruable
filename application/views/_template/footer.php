4551d565bdf335ff5f817116518eb51f47fed862
candraagungpurnomo <122202002820@mhs.dinus.ac.id>
2024-04-12 10:52:51 +0700

    Template-Guruable

---
+++ application/views/_template/footer.php
@@ -0,0 +1,33 @@
+<!-- Footer -->
+<!-- <footer class="sticky-footer bg-info">
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+            <span>Copyright &copy; FOOTER-All Rights Reserved.</span><br><br>
+            <span>Didukung Oleh : </span><br>
+            <!-- <span>
+                <img src="<?= base_url('vendors/images/logopendidikan.png') ?>" alt="Logo Tutwurihandayani" class="img-fluid">
+                <img src="<?= base_url('vendors/images/logoudinus1.png') ?>" alt="Logo Udinus" class="img-fluid">
+                <img src="<?= base_url('vendors/images/logokendal1.png') ?>" alt=" Logo Kendal" class="img-fluid">
+            </span> -->
+        <!-- </div>
+    </div>
+</footer> --> 
+<!-- End of Footer -->
+<script type="text/javascript" src="<?php echo base_url('assets/js/jquery/jquery.min.js') ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/jquery-ui/jquery-ui.min.js') ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/popper.js/popper.min.js') ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/bootstrap/js/bootstrap.min.js') ?>"></script>
+<!-- jquery slimscroll js -->
+<script type="text/javascript" src="<?php echo base_url('assets/js/jquery-slimscroll/jquery.slimscroll.js') ?>"></script>
+<!-- modernizr js -->
+<script type="text/javascript" src="<?php echo base_url('assets/js/modernizr/modernizr.js') ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/modernizr/css-scrollbars.js') ?>"></script>
+<!-- classie js -->
+<script type="text/javascript" src="<?php echo base_url('assets/js/classie/classie.js') ?>"></script>
+<!-- Custom js -->
+<script type="text/javascript" src="<?php echo base_url('assets/js/script.js') ?>"></script>
+<script src="<?php echo base_url('assets/js/pcoded.min.js') ?>"></script>
+<script src="<?php echo base_url('assets/js/demo-12.js') ?>"></script>
+<script src="<?php echo base_url('assets/js/jquery.mCustomScrollbar.concat.min.js') ?>"></script>
+</body>
+</html>
---
+++ application/views/user.php
@@ -0,0 +1,58 @@
+                    <div class="pcoded-content">
+                        <div class="pcoded-inner-content">
+                            <!-- Main-body start -->
+                            <div class="main-body">
+                                <div class="page-wrapper">
+                                    <!-- Page-header start -->
+                                    <div class="page-header card">
+                                        <div class="row align-items-end">
+                                            <div class="col-lg-8">
+                                                <div class="page-header-title">
+                                                    <i class="icofont icofont-table bg-c-blue"></i>
+                                                    <div class="d-inline">
+                                                        <h4>Bootstrap Basic Tables</h4>
+                                                        <span>lorem ipsum dolor sit amet, consectetur adipisicing elit</span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-4">
+                                                <div class="page-header-breadcrumb">
+                                                   <ul class="breadcrumb-title">
+                                                    <li class="breadcrumb-item">
+                                                        <a href="index.html">
+                                                            <i class="icofont icofont-home"></i>
+                                                        </a>
+                                                    </li>
+                                                    <li class="breadcrumb-item"><a href="#!">Bootstrap Table</a>
+                                                    </li>
+                                                    <li class="breadcrumb-item"><a href="#!">Basic Table</a>
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- Page-header end -->
+                                
+                                <!-- Page-body start -->
+                                <!-- Page-body end -->
+                            </div>
+                        </div>
+                        <!-- Main-body end -->
+                        
+
+                        <div id="styleSelector">
+
+                        </div>
+                    </div>
+                </div> <!-- TO USER END -->
+            </div><!-- SIDEBAR pcoded-wrapper -->
+        </div><!-- SIDEBAR pcoded-main -->
+
+                <div class="fixed-button">
+                    <a href="https://codedthemes.com/item/guru-able-admin-template/" target="_blank" class="btn btn-md btn-primary">
+                      <i class="fa fa-shopping-cart" aria-hidden="true"></i> Upgrade To Pro
+                    </a>
+                </div>
+            </div>  <!-- PAGE HEADER -->
+        </div> <!-- PAGE HEADER -->
